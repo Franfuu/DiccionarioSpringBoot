ed5e1e15923146a91a3de758073d21bee5b35436
Franfuu <franperezruiz10@gmail.com>
2025-03-03 13:07:07 +0100

    Primera toma de contacto

---
+++ src/main/java/com/github/franfuu/springdiccionario/SpringDiccionarioApplication.java
@@ -0,0 +1,13 @@
+package com.github.franfuu.springdiccionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiccionarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDiccionarioApplication.class, args);
+    }
+
+}
